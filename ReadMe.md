f87d9d569453e8ba4def3c4559e0fd1c79a38e57
dapclark <dapclark@gmail.com>
2017-02-05 17:29:48 -0600

    Final updates

--- ReadMe.md
+++ ReadMe.md
@@ -34,12 +34,18 @@ The assignment is to "create one R script called run_analysis.R that does the fo
 4. Read in the column names from "features.txt" and assign them to the columns.
 
 ## 2. Extracts only the measurements on the mean and standard deviation for each measurement.
-
+1. Create a subset of all of the feature names using grep; include only those features that have "mean()" or "std()"
+2. Use the subset of names to subset the actual columns of the complete data set.
 
 ## 3. Uses descriptive activity names to name the activities in the data set
 1. Use the "factor" function to convert activity numbers to their matching character labels.
 
 ## 4. Appropriately labels the data set with descriptive variable names.
-
+1. Create a temporary variable to store the list of names in the current data set.
+2. Use grep to search and replace. Improve readability and findability by spelling out all abbreviations, removing parentheses, and using lower case.
+3. Copy temporary variable back into the column headers.
 
 ## 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+1. Group data set by subject number and by activity.
+2. Sort by subject and then by activity.
+3. Write new data set out to "tidydata.txt"
--- runanalysis.R
+++ runanalysis.R
@@ -1,3 +1,5 @@
+library(plyr)
+
 ## 1. Merges the training and the test sets to create one data set.
 
 ## Import and combine subject data (and name the column ("subject")
@@ -15,21 +17,48 @@ test_features <- read.table("UCI HAR Dataset/test/X_test.txt", header = FALSE)
 train_features <- read.table("UCI HAR Dataset/train/X_train.txt", header = FALSE)
 all_features <- rbind(test_features, train_features)
 
-## Apply features names to features columns
+## Import names of features and rename the features columns
 features_names <- read.table("UCI HAR Dataset/features.txt", header = FALSE)
 names(all_features) <- features_names$V2
 
 ## Combine activity, subject, and features into a single dataframe
 all_data <- cbind (all_subject, all_activity, all_features)
 
+
 ## 2. Extracts only the measurements on the mean and standard deviation for each measurement.
 
+## Create a subset of the full variable set by using grep to identify varibles with "mean()" or "std()"
+subset_features_names <- features_names$V2[grep("mean\\(\\)|std\\(\\)", features_names$V2)]
+subset_columns <-c("subject", "activity", as.character(subset_features_names))
+all_data <- subset(all_data, select = subset_columns)
 
 
 ## 3. Uses descriptive activity names to name the activities in the data set
-all_activity$activity <- factor(all_activity$activity, levels = c(1,2,3,4,5,6), labels = c("walking", "walking_upstairs", "walking_downstairs", "sitting", "standing", "laying"))
+all_data$activity <- factor(all_data$activity, levels = c(1,2,3,4,5,6), labels = c("walking", "walking_upstairs", "walking_downstairs", "sitting", "standing", "laying"))
+
 
 ## 4. Appropriately labels the data set with descriptive variable names.
 
+## Assign column names to a temp variable, then use grep to eliminate abbreviations, parentheses, and capitalization.
+names_temp <- names(all_data)
+names_temp <- gsub("\\(\\)", "", names_temp)
+names_temp <- gsub("BodyBody", "-body", names_temp)
+names_temp <- gsub("^t", "time", names_temp)
+names_temp <- gsub("^f", "frequency", names_temp)
+names_temp <- gsub("Acc", "-acceleration", names_temp)
+names_temp <- gsub("Gyro", "-gyro", names_temp)
+names_temp <- gsub("Jerk", "-jerk", names_temp)
+names_temp <- gsub("Body", "-body", names_temp)
+names_temp <- gsub("Gravity", "-gravity", names_temp)
+names_temp <- gsub("Mag", "-magnitude", names_temp)
+names_temp <- tolower (names_temp)
+names(all_data) <- names_temp
+
 
 ## 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+## Create a second data set, grouped data by subject and activity and sorted by subject then activity.
+library(plyr);
+all_data_2 <- aggregate(. ~subject + activity, all_data, mean)
+all_data_2 <- all_data_2[order(all_data_2$subject,all_data_2$activity),]
+write.table(all_data_2, file = "tidydata.txt",row.name=FALSE)
